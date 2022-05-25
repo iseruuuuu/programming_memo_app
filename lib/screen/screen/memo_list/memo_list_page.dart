74a774bfb334110154896e98d1ce3f34b76ea813
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-05-25 18:58:42 +0900

    sort: sort indent

---
+++ lib/screen/screen/memo_list/memo_list_page.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:programming_memo_for_mac_app/screen/container/memo_list/parts/deleted_memo_list.dart';
+import 'package:programming_memo_for_mac_app/screen/container/memo_list/parts/memo_list_widgets_container.dart';
+import 'package:programming_memo_for_mac_app/screen/screen/error_text_widget.dart';
+import 'package:programming_memo_for_mac_app/store/loading_state.dart';
+
+class MemoListPage extends StatelessWidget {
+  final Function() createMemo;
+  final Function() reloadMemo;
+  final LoadingState loadingState;
+
+  const MemoListPage(
+      {required this.loadingState,
+      required this.createMemo,
+      required this.reloadMemo,
+      Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final memoListFailedToLoad =
+        AppLocalizations.of(context)?.memoListFailedToLoad ?? "";
+    final memoListTitle = AppLocalizations.of(context)?.memoListTitle ?? "";
+    final memoListTabList = AppLocalizations.of(context)?.memoListTabList ?? "";
+    final memoListTabDeletedList =
+        AppLocalizations.of(context)?.memoListTabDeletedList ?? "";
+
+    final errorWidget = ErrorTextWidget(
+      errorText: memoListFailedToLoad,
+      recoverActionText: "再読み込み",
+      onActionPressed: () {
+        reloadMemo();
+      },
+    );
+
+    Future<bool> _willPopCallback() async {
+      return true;
+    }
+
+    return WillPopScope(
+      onWillPop: _willPopCallback,
+      child: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(memoListTitle),
+            bottom: TabBar(
+              tabs: <Widget>[
+                Tab(text: memoListTabList),
+                Tab(text: memoListTabDeletedList),
+              ],
+            ),
+            automaticallyImplyLeading: false,
+          ),
+          body: TabBarView(
+            children: <Widget>[
+              //TODO メモ一覧
+              MemoListWidgetsContainer(errorWidget: errorWidget),
+              //TODO 削除済みメモ一覧
+              DeletedMemoListWidgetsContainer(errorWidget: errorWidget),
+            ],
+          ),
+          floatingActionButton: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              FloatingActionButton(
+                heroTag: "createMemo",
+                child: const Icon(Icons.add),
+                onPressed: createMemo,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
