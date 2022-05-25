d17e3f4562a5eb7bbca53af6cbb8cc28666c115a
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-05-25 15:00:26 +0900

    sort: fix indent

--- lib/screen/container/memo/parts/markdown_body.dart
+++ lib/screen/container/memo/parts/markdown_body.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:programming_memo_for_mac_app/screen/component/memo/parts/markdown_builder/header.dart';
 import 'package:url_launcher/url_launcher.dart';
-
-import '../../../component/memo/parts/markdown_builder/header.dart';
 
 class MarkdownBodyHeaderCopiableContainer extends StatelessWidget {
   final String content;
@@ -24,19 +23,43 @@ class MarkdownBodyHeaderCopiableContainer extends StatelessWidget {
     final _onCopyRequested = onCopyRequested;
     return MarkdownBody(
       data: content,
-      onTapLink: (_, url, __){
+      onTapLink: (_, url, __) {
         if (url != null) {
           final uri = Uri.parse(url);
           launchUrl(uri);
         }
       },
       builders: {
-        'h1': CustomHeader1Builder(content: content, onHeaderFound: add, onCopyRequested: _onCopyRequested),
-        'h2': CustomHeader2Builder(content: content, onHeaderFound: add, onCopyRequested: _onCopyRequested),
-        'h3': CustomHeader3Builder(content: content, onHeaderFound: add, onCopyRequested: _onCopyRequested),
-        'h4': CustomHeader4Builder(content: content, onHeaderFound: add, onCopyRequested: _onCopyRequested),
-        'h5': CustomHeader5Builder(content: content, onHeaderFound: add, onCopyRequested: _onCopyRequested),
-        'h6': CustomHeader6Builder(content: content, onHeaderFound: add, onCopyRequested: _onCopyRequested)
+        'h1': CustomHeader1Builder(
+          content: content,
+          onHeaderFound: add,
+          onCopyRequested: _onCopyRequested,
+        ),
+        'h2': CustomHeader2Builder(
+          content: content,
+          onHeaderFound: add,
+          onCopyRequested: _onCopyRequested,
+        ),
+        'h3': CustomHeader3Builder(
+          content: content,
+          onHeaderFound: add,
+          onCopyRequested: _onCopyRequested,
+        ),
+        'h4': CustomHeader4Builder(
+          content: content,
+          onHeaderFound: add,
+          onCopyRequested: _onCopyRequested,
+        ),
+        'h5': CustomHeader5Builder(
+          content: content,
+          onHeaderFound: add,
+          onCopyRequested: _onCopyRequested,
+        ),
+        'h6': CustomHeader6Builder(
+          content: content,
+          onHeaderFound: add,
+          onCopyRequested: _onCopyRequested,
+        )
       },
     );
   }
