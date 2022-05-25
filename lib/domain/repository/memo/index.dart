1b0c9a1609998fd7d29f403053d096a4166f55d8
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-05-25 16:51:07 +0900

    add: 初期値の値を空にした・インデントの整理

--- lib/domain/repository/memo/index.dart
+++ lib/domain/repository/memo/index.dart
@@ -1,19 +1,15 @@
+import 'package:programming_memo_for_mac_app/config.dart';
 import 'package:programming_memo_for_mac_app/domain/entities/memo/memo.dart';
 import 'package:programming_memo_for_mac_app/domain/repository/memo/interface.dart';
 import 'package:programming_memo_for_mac_app/infra/persistence/database/interface.dart';
+import 'package:programming_memo_for_mac_app/libs/id_generator/id_generator.dart';
 import 'package:universal_io/io.dart';
-import '../../../config.dart';
-import '../../../libs/id_generator/id_generator.dart';
 
-
-class MemoHiveRepository implements MemoRepository{
+class MemoHiveRepository implements MemoRepository {
   final PersistenceDatabase database;
   final IdGenerator idGenerator;
 
-  MemoHiveRepository({
-    required this.database,
-    required this.idGenerator
-  });
+  MemoHiveRepository({required this.database, required this.idGenerator});
 
   @override
   Future<Memo?> loadMemo(String memoId) {
@@ -25,7 +21,10 @@ class MemoHiveRepository implements MemoRepository{
     final memos = await database.loadAll<Memo>();
     final undeletedMemos = memos.where((m) => m.willDeleteAt == null).toList();
     undeletedMemos.sort(
-            (a, b) => a.lastModifiedAt.millisecondsSinceEpoch > b.lastModifiedAt.millisecondsSinceEpoch ? -1 : 1
+      (a, b) => a.lastModifiedAt.millisecondsSinceEpoch >
+              b.lastModifiedAt.millisecondsSinceEpoch
+          ? -1
+          : 1,
     );
     return undeletedMemos;
   }
@@ -40,13 +39,13 @@ class MemoHiveRepository implements MemoRepository{
     final now = DateTime.now();
     if (prevMemo == null) {
       final newMemo = Memo(
-          id: memo.id,
-          createdAt: now,
-          lastOpenedAt: now,
-          lastModifiedAt: now,
-          willDeleteAt: null,
-          title: memo.title,
-          content: memo.content
+        id: memo.id,
+        createdAt: now,
+        lastOpenedAt: now,
+        lastModifiedAt: now,
+        willDeleteAt: null,
+        title: memo.title,
+        content: memo.content,
       );
       return newMemo;
     } else {
@@ -55,7 +54,7 @@ class MemoHiveRepository implements MemoRepository{
         lastOpenedAt: memo.lastOpenedAt,
         willDeleteAt: memo.willDeleteAt,
         title: memo.title,
-        content: memo.content
+        content: memo.content,
       );
       return newMemo;
     }
@@ -78,84 +77,75 @@ class MemoHiveRepository implements MemoRepository{
     final locale = Platform.localeName;
     final title = (() {
       if (locale.contains("en")) {
-        return "Sample memo";
+        return "";
       }
-      return "サンプルメモ";
+      return "";
     })();
     final content = (() {
       if (locale.contains("en")) {
-        return '''
-# Sample heading
-## test
-Test input. You can write memo with the Markdown format(GFM).
-
-You also able to write multiline with  
-trailing two spaces.
-
-[Wikipedia](https://www.wikipedia.org/)
-      ''';
+        return "";
       }
-      return '''
-# サンプル見出し
-## test
-テスト入力です。Markdown形式(GFM準拠)でメモをとることができます。
-
-文の末尾に半角スペースを入れると、  
-改行できます。
-
-[Wikipedia](https://www.wikipedia.org/)
-      ''';
+      return "";
     })();
     final now = DateTime.now();
-    return Future<Memo>.value(Memo(
-      id: idGenerator.generate(),
-      title: title,
-      content: content,
-      createdAt: now,
-      lastModifiedAt: now,
-      lastOpenedAt: now,
-      willDeleteAt: null,
-    ));
+    return Future<Memo>.value(
+      Memo(
+        id: idGenerator.generate(),
+        title: title,
+        content: content,
+        createdAt: now,
+        lastModifiedAt: now,
+        lastOpenedAt: now,
+        willDeleteAt: null,
+      ),
+    );
   }
 
   @override
   Future<Memo> updateOpenedAt(Memo memo) {
-    return Future<Memo>.value(memo.copyWith(
-      lastOpenedAt: DateTime.now(),
-    ));
+    return Future<Memo>.value(
+      memo.copyWith(
+        lastOpenedAt: DateTime.now(),
+      ),
+    );
   }
 
   @override
   Future<Memo> makeDeleteMemo(Memo memo) {
     final willDeleteAt =
-      DateTime.now().add(const Duration(days: Constants.memoWillDeleteDays));
-    return Future<Memo>.value(memo.copyWith(
-      willDeleteAt: willDeleteAt,
-    ));
+        DateTime.now().add(const Duration(days: Constants.memoWillDeleteDays));
+    return Future<Memo>.value(
+      memo.copyWith(
+        willDeleteAt: willDeleteAt,
+      ),
+    );
   }
 
   @override
   Future<Memo> updateModifiedAt(Memo memo) {
-    return Future<Memo>.value(memo.copyWith(
-      lastModifiedAt: DateTime.now(),
-    ));
+    return Future<Memo>.value(
+      memo.copyWith(
+        lastModifiedAt: DateTime.now(),
+      ),
+    );
   }
 
   @override
   Future<Memo> restoreMemo(Memo memo) {
-    return Future<Memo>.value(memo.copyWith(
-        willDeleteAt: null
-    ));
+    return Future<Memo>.value(
+      memo.copyWith(
+        willDeleteAt: null,
+      ),
+    );
   }
 
   @override
   Future<void> deleteExpiredMemos() async {
     final deletedMemos = await loadDeletedMemos();
     final now = DateTime.now();
-    final expiredMemos =
-      deletedMemos.where((m) =>
-        m.willDeleteAt?.isBefore(now) == true
-      );
+    final expiredMemos = deletedMemos.where(
+      (m) => m.willDeleteAt?.isBefore(now) == true,
+    );
     for (final memo in expiredMemos) {
       deleteMemo(memo);
     }
