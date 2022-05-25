5677565122675c5908573192ec44bc62293df08a
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-05-25 18:55:15 +0900

    sort: sort indent

--- lib/screen/container/memo_list/memo_list_page_container.dart
+++ lib/screen/container/memo_list/memo_list_page_container.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:programming_memo_for_mac_app/screen/component/memo_list/memo_list_page.dart';
-import '../../../store/memo/memo_provider.dart';
-
+import 'package:programming_memo_for_mac_app/screen/screen/memo_list/memo_list_page.dart';
+import 'package:programming_memo_for_mac_app/store/memo/memo_provider.dart';
 
 class MemoListPageContainer extends ConsumerWidget {
   const MemoListPageContainer({Key? key}) : super(key: key);
