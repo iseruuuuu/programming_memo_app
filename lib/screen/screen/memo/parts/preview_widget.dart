57d0ba58440dc0da29778fdc29817f0c8c253e7c
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-05-25 18:58:09 +0900

    sort: sort indent

--- lib/screen/screen/memo/parts/preview_widget.dart
+++ lib/screen/screen/memo/parts/preview_widget.dart
@@ -1,12 +1,9 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-
-
-import '../../../../store/loading_state.dart';
-import '../../../container/memo/parts/markdown_body.dart';
-import '../../loading.dart';
+import 'package:programming_memo_for_mac_app/screen/container/memo/parts/markdown_body.dart';
+import 'package:programming_memo_for_mac_app/screen/screen/loading.dart';
+import 'package:programming_memo_for_mac_app/store/loading_state.dart';
 
 class PreviewWidget extends StatelessWidget {
   const PreviewWidget({
@@ -14,7 +11,7 @@ class PreviewWidget extends StatelessWidget {
     required this.content,
     required this.loadingState,
     required this.onCopyRequested,
-    Key? key
+    Key? key,
   }) : super(key: key);
   final String? title;
   final String? content;
@@ -23,26 +20,35 @@ class PreviewWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final notifyMemoContentCopied = AppLocalizations.of(context)?.notifyMemoContentCopied ?? "";
+    final memoTitleName = AppLocalizations.of(context)?.memoTitleName ?? "";
+    final memoContentName = AppLocalizations.of(context)?.memoContentName ?? "";
+    final notifyMemoContentCopied =
+        AppLocalizations.of(context)?.notifyMemoContentCopied ?? "";
     final memoListLoading = AppLocalizations.of(context)?.memoListLoading ?? "";
-
     const externalPaddingInset = EdgeInsets.all(8);
     const internalPaddingInset = EdgeInsets.all(10);
     const titleTextStyle = TextStyle(
-        fontStyle: FontStyle.italic,
-        fontSize: 24
+      fontStyle: FontStyle.italic,
+      fontSize: 24,
+    );
+    const titleStyle = TextStyle(
+      fontWeight: FontWeight.bold,
+      color: Colors.black,
+      fontSize: 30,
     );
 
-    void _onCopyRequested (String text) {
+    void _onCopyRequested(String text) {
       onCopyRequested(text);
-      final textSummary = text.substring(0, min(16, text.length)).replaceAll("\n", "");
+      final textSummary =
+          text.substring(0, min(16, text.length)).replaceAll("\n", "");
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
         content: Text("$notifyMemoContentCopied ($textSummary...)"),
         duration: const Duration(seconds: 2),
       ));
     }
 
-    if (loadingState == LoadingState.initial || loadingState == LoadingState.loading) {
+    if (loadingState == LoadingState.initial ||
+        loadingState == LoadingState.loading) {
       return const LoadingWidget();
     }
     return SingleChildScrollView(
@@ -52,8 +58,25 @@ class PreviewWidget extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
+              memoTitleName,
+              style: titleStyle,
+            ),
+            const Divider(
+              color: Colors.black,
+              thickness: 2,
+            ),
+            Text(
               title ?? memoListLoading,
               style: titleTextStyle,
+            ),
+            const Padding(padding: internalPaddingInset),
+            Text(
+              memoContentName,
+              style: titleStyle,
+            ),
+            const Divider(
+              color: Colors.black,
+              thickness: 2,
             ),
             const Padding(padding: internalPaddingInset),
             MarkdownBodyHeaderCopiableContainer(
@@ -61,8 +84,8 @@ class PreviewWidget extends StatelessWidget {
               onCopyRequested: _onCopyRequested,
             ),
           ],
-        )
-      )
+        ),
+      ),
     );
   }
 }
