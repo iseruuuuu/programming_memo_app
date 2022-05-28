5a1d3fdcaa6b8ac2e3236b9c3c835a45af5f3a8f
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-05-28 20:29:37 +0900

    Add: consoleに出てくるものを表示させないようにした

--- lib/logger/logger.dart
+++ lib/logger/logger.dart
@@ -1,21 +1,11 @@
-import 'package:flutter/foundation.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class ProviderLogger extends ProviderObserver {
   @override
   void didUpdateProvider(
-      ProviderBase provider,
-      Object? previousValue,
-      Object? newValue,
-      ProviderContainer container,
-      ) {
-    if (kDebugMode) {
-      print('''
-{
-  "provider": "${provider.name ?? provider.runtimeType}",
-  "newValue": "$newValue"
-}'''
-      );
-    }
-  }
+    ProviderBase provider,
+    Object? previousValue,
+    Object? newValue,
+    ProviderContainer container,
+  ) {}
 }
