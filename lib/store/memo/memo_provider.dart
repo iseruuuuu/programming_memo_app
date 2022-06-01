9d3d6399e097a0af159cdffcadc63c823c0d0f33
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-06-01 11:04:34 +0900

    sort: importのsortやコードの整理を行った

--- lib/store/memo/memo_provider.dart
+++ lib/store/memo/memo_provider.dart
@@ -1,13 +1,11 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import '../../container.dart';
-import '../../domain/entities/memo/memo.dart';
-import '../../usecase/memo/memo.dart';
-import '../loading_state.dart';
+import 'package:programming_memo_for_mac_app/container.dart';
+import 'package:programming_memo_for_mac_app/domain/entities/memo/memo.dart';
+import 'package:programming_memo_for_mac_app/store/loading_state.dart';
+import 'package:programming_memo_for_mac_app/usecase/memo/memo.dart';
 import 'memo_state.dart';
 
-final memoStateProvider =
-StateNotifierProvider<EditorNotifier, MemoState>(
+final memoStateProvider = StateNotifierProvider<EditorNotifier, MemoState>(
   (ref) => EditorNotifier(memoUseCase),
 );
 
@@ -48,15 +46,11 @@ class EditorNotifier extends StateNotifier<MemoState> {
   }
 
   void updateTitle(Memo memo, String title) {
-    _setMemo(memo.copyWith(
-        title: title
-    ));
+    _setMemo(memo.copyWith(title: title));
   }
 
   void updateContent(Memo memo, String content) {
-    _setMemo(memo.copyWith(
-        content: content
-    ));
+    _setMemo(memo.copyWith(content: content));
   }
 
   void storeMemo(Memo memo, List<Memo> memoList) async {
@@ -68,7 +62,7 @@ class EditorNotifier extends StateNotifier<MemoState> {
         ...[...memoList]..removeWhere((m) => m.id == memo.id)
       ];
       _setMemoList(replacedMemoList);
-    } catch(e) {
+    } catch (e) {
       // TODO(Niccari): Error handling
     }
   }
@@ -86,15 +80,17 @@ class EditorNotifier extends StateNotifier<MemoState> {
       // foundMemoIndex must be >= 0.
       newMemoList[foundMemoIndex] = newMemo;
       _setMemoAndMemoList(newMemo, newMemoList);
-    } catch(e) {
+    } catch (e) {
       // TODO(Niccari): Error handling
     }
   }
 
-  void makeDeleteMemo(Memo memo, List<Memo> memoList, List<Memo> deletedMemoList) async {
+  void makeDeleteMemo(
+      Memo memo, List<Memo> memoList, List<Memo> deletedMemoList) async {
     try {
       final newMemo = await memoUseCase.makeDeleteMemo(memo);
-      final removedMemoList = [...memoList]..removeWhere((m) => m.id == memo.id);
+      final removedMemoList = [...memoList]
+        ..removeWhere((m) => m.id == memo.id);
       final newDeletedMemoList = [newMemo, ...deletedMemoList];
       if (removedMemoList.isEmpty) {
         _setMemoAll(null, removedMemoList, newDeletedMemoList);
@@ -102,19 +98,18 @@ class EditorNotifier extends StateNotifier<MemoState> {
       } else {
         _setMemoAll(removedMemoList.first, removedMemoList, newDeletedMemoList);
       }
-    } catch(e) {
+    } catch (e) {
       // TODO(Niccari): Error handling
     }
   }
 
-  void restoreMemo(Memo memo, List<Memo> memoList, List<Memo> deletedMemoList) async {
+  void restoreMemo(
+      Memo memo, List<Memo> memoList, List<Memo> deletedMemoList) async {
     try {
       final newMemo = await memoUseCase.restoreMemo(memo);
-      _setMemoListAll(
-          [newMemo, ...memoList],
-          [...deletedMemoList]..removeWhere((m) => m.id == newMemo.id)
-      );
-    } catch(e) {
+      _setMemoListAll([newMemo, ...memoList],
+          [...deletedMemoList]..removeWhere((m) => m.id == newMemo.id));
+    } catch (e) {
       // TODO(Niccari): Error handling
     }
   }
@@ -122,44 +117,60 @@ class EditorNotifier extends StateNotifier<MemoState> {
   void permanentDeleteMemo(Memo memo, List<Memo> deletedMemoList) async {
     try {
       await memoUseCase.deleteMemo(memo);
-      _setDeletedMemoList([...deletedMemoList]..removeWhere((m) => m.id == memo.id));
-    } catch(e) {
+      _setDeletedMemoList(
+          [...deletedMemoList]..removeWhere((m) => m.id == memo.id));
+    } catch (e) {
       // TODO(Niccari): Error handling
     }
   }
 
   // Reducer
-  void _setLoadingState(LoadingState loadingState) => state = state.copyWith(
-    loadingState: loadingState,
-  );
+  void _setLoadingState(LoadingState loadingState) {
+    state = state.copyWith(
+      loadingState: loadingState,
+    );
+  }
 
-  void _setMemo(Memo memo) => state = state.copyWith(
-    memo: memo,
-  );
+  void _setMemo(Memo memo) {
+    state = state.copyWith(
+      memo: memo,
+    );
+  }
 
-  void _setDeletedMemoList(List<Memo> deletedMemoList) => state = state.copyWith(
-    deletedMemoList: deletedMemoList,
-  );
+  void _setDeletedMemoList(List<Memo> deletedMemoList) {
+    state = state.copyWith(
+      deletedMemoList: deletedMemoList,
+    );
+  }
 
-  void _setMemoList(List<Memo> memoList) => state = state.copyWith(
-    memoList: memoList,
-  );
+  void _setMemoList(List<Memo> memoList) {
+    state = state.copyWith(
+      memoList: memoList,
+    );
+  }
 
-  void _setMemoAndMemoList(Memo? memo, List<Memo> memoList) => state = state.copyWith(
-    memo: memo,
-    memoList: memoList,
-  );
+  void _setMemoAndMemoList(Memo? memo, List<Memo> memoList) {
+    state = state.copyWith(
+      memo: memo,
+      memoList: memoList,
+    );
+  }
 
-  void _setMemoListAll(List<Memo> memoList, List<Memo> deletedMemoList) => state = state.copyWith(
-    memoList: memoList,
-    deletedMemoList: deletedMemoList
-  );
+  void _setMemoListAll(List<Memo> memoList, List<Memo> deletedMemoList) {
+    state = state.copyWith(
+      memoList: memoList,
+      deletedMemoList: deletedMemoList,
+    );
+  }
 
-  void _setMemoAll(Memo? memo, List<Memo> memoList, List<Memo> deletedMemoList) => state = state.copyWith(
-    memo: memo,
-    memoList: memoList,
-    deletedMemoList: deletedMemoList
-  );
+  void _setMemoAll(
+      Memo? memo, List<Memo> memoList, List<Memo> deletedMemoList) {
+    state = state.copyWith(
+      memo: memo,
+      memoList: memoList,
+      deletedMemoList: deletedMemoList,
+    );
+  }
 }
 
 //TODO indexエラーでの解決策
--- lib/store/memo/memo_state.dart
+++ lib/store/memo/memo_state.dart
@@ -1,26 +1,24 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-
-import '../../domain/entities/memo/memo.dart';
-import '../loading_state.dart';
+import 'package:programming_memo_for_mac_app/domain/entities/memo/memo.dart';
+import 'package:programming_memo_for_mac_app/store/loading_state.dart';
 
 part 'memo_state.freezed.dart';
 
 @freezed
 class MemoState with _$MemoState {
-  const factory MemoState(
-      {
-        required LoadingState loadingState,
-        required Memo? memo,
-        required List<Memo> memoList,
-        required List<Memo> deletedMemoList,
-      }) = _MemoState;
+  const factory MemoState({
+    required LoadingState loadingState,
+    required Memo? memo,
+    required List<Memo> memoList,
+    required List<Memo> deletedMemoList,
+  }) = _MemoState;
 
   factory MemoState.initial() {
     return const MemoState(
-        loadingState: LoadingState.initial,
-        memo: null,
-        memoList: [],
-        deletedMemoList: []
+      loadingState: LoadingState.initial,
+      memo: null,
+      memoList: [],
+      deletedMemoList: [],
     );
   }
 }
